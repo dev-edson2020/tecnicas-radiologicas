88904027e62e93f07402fcbf394f700665ad3b32
Edson Meira <edsonanga@hotmail.com>
2025-05-29 16:56:27 -0300

    Correção app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Technique } from './models/technique';
 import { HeaderComponent } from './components/header/header.component'; // ajuste o caminho se necessário
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
-  template: `
-    <app-header (novaTecnicaSalva)="onNovaTecnicaSalva($event)" />
+  template: `  
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
-  title = 'Sua Aplicação';
-  selectedTechnique?: Technique;
-
-  onNovaTecnicaSalva(t: Technique) {
-    this.selectedTechnique = t;
-    console.log('Técnica recebida no AppComponent:', t);
-  }
 }
