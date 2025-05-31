caf39975e41777c10354b87983f866191b1092b8
Edson Meira <edsonanga@hotmail.com>
2025-05-31 09:06:59 -0300

    Ajustes no card

--- src/app/components/technique-card/technique-card.component.ts
+++ src/app/components/technique-card/technique-card.component.ts
@@ -4,24 +4,29 @@ import {
   Input,
   Output,
   OnChanges,
-  SimpleChanges
+  SimpleChanges,
+  ViewChild,
+  ElementRef
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Technique } from '../../models/technique';
+import {MatCardModule} from '@angular/material/card';
 import { TechniqueService } from '../../services/technique.service';
 
 @Component({
   selector: 'app-technique-card',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatCardModule],
   templateUrl: './technique-card.component.html',
   styleUrls: ['./technique-card.component.scss']
 })
 export class TechniqueCardComponent implements OnChanges {
+
+  @ViewChild('kvInput') kvInput!: ElementRef;
+
   @Input() technique!: Technique;
   @Output() onDelete = new EventEmitter<number>();
-
 
   isEditing = false;
   localTechnique!: Technique;
@@ -37,30 +42,33 @@ export class TechniqueCardComponent implements OnChanges {
 
   toggleEdit() {
     this.isEditing = true;
-  }
-
-  saveChanges() {
-    const updatedTechnique: Technique = {
-      ...this.localTechnique,
-      id: this.technique.id,
-      category: this.technique.category
-    };
-
-    this.techniqueService.updateTechnique(this.technique.id, updatedTechnique).subscribe({
-      next: updated => {
-        this.technique = updated;
-        this.localTechnique = { ...updated };
-        this.isEditing = false;
-      },
-      error: err => {
-        console.error('Erro ao atualizar técnica:', err);
-        alert('Erro ao salvar alterações. Tente novamente.');
-      }
+    setTimeout(() => {
+      this.kvInput?.nativeElement?.focus();
     });
   }
 
-  cancelEdit() {
-    this.localTechnique = { ...this.technique };
-    this.isEditing = false;
-  }
+saveChanges() {
+  const updatedTechnique: Technique = {
+    ...this.localTechnique,
+    id: this.technique.id,
+    category: this.technique.category
+  };
+
+  this.techniqueService.updateTechnique(this.technique.id, updatedTechnique).subscribe({
+    next: updated => {
+      this.technique = updated;
+      this.localTechnique = { ...updated };
+      this.isEditing = false;
+    },
+    error: err => {
+      console.error('Erro ao atualizar técnica:', err);
+      alert('Erro ao salvar alterações. Tente novamente.');
+    }
+  });
+}
+
+cancelEdit() {
+  this.localTechnique = { ...this.technique };
+  this.isEditing = false;
+}
 }
